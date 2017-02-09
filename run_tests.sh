8d467498446fc8be7811bbffe4d589f22634b656
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2017-02-09 14:45:59 +0700

    use import.bash instead of submodule

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,9 @@
 set -euo pipefail
 
 _base_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-source "$_base_dir/vendor/github.com/reconquest/test-runner.bash/test-runner.bash"
+source "$_base_dir/vendor/github.com/reconquest/import.bash/import.bash"
+
+import:use github.com/reconquest/test-runner.bash
 
 _tmux_session="tmux-test-$RANDOM"
 
